215e98e8c103bc29d7036056e816690aebe01e12
Osman Enayat <94260380+AKA-Eloquence@users.noreply.github.com>
2022-02-15 04:56:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,32 @@
 // litening for click on ancher tags
 const propertyType = document.querySelectorAll('a');
 const property = document.querySelectorAll('.second-slider');
+const nearbyLocations = document.querySelectorAll('.nearby-slider');
 
 for (let i = 0; i < propertyType.length; i++) {
     propertyType[i].addEventListener('click', function(){
 
+        // Button Activation
         classRemove (propertyType,'active-property-type');
         this.classList.add('active-property-type');
 
         // changing slider
-        const activePropertySlider = document.querySelectorAll('#'+this.innerHTML);
-
-        // manupulating slider
+        const activePropertySlider = document.querySelectorAll('#'+this.innerText);
+        // manipulating slider
         classRemove (property , 'active-slider');
-        classAdd (property , 'not-active-slider')
-
+        classAdd (property , 'not-active-slider');
         // manipulating selected slider
         classRemove (activePropertySlider , 'not-active-slider');
         classAdd (activePropertySlider , 'active-slider')
+        // Changing Nearby Locations
+        const activeNearbyLocation = document.querySelectorAll('.'+this.innerText);
+        //  manipulating Nearby Locations
+        classRemove (nearbyLocations , 'active-slider');
+        classAdd (nearbyLocations, 'not-active-slider');
+        // manipulating selected slider
+        classRemove (activeNearbyLocation , 'not-active-slider');
+        classAdd (activeNearbyLocation , 'active-slider');
+        console.log(nearbyLocations);
     });
   };
 
