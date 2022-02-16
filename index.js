192a8bf6934438cd78b0e3022c98fa63ed68c2bb
Osman Enayat <94260380+AKA-Eloquence@users.noreply.github.com>
2022-02-16 06:34:55 -0800

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,40 @@ for (let i = 0; i < propertyType.length; i++) {
     propertyType[i].addEventListener('click', function(){
 
         // Button Activation
-        classRemove (propertyType,'active-property-type');
+        myClass.removeCla(propertyType,'active-property-type');
         this.classList.add('active-property-type');
 
         // changing slider
         const activePropertySlider = document.querySelectorAll('#'+this.innerText);
         // manipulating slider
-        classRemove (property , 'active-slider');
-        classAdd (property , 'not-active-slider');
+        myClass.removeCla(property , 'active-slider');
+        myClass.addCla(property , 'not-active-slider');
         // manipulating selected slider
-        classRemove (activePropertySlider , 'not-active-slider');
-        classAdd (activePropertySlider , 'active-slider')
+        myClass.removeCla(activePropertySlider , 'not-active-slider');
+        myClass.addCla(activePropertySlider , 'active-slider')
         // Changing Nearby Locations
         const activeNearbyLocation = document.querySelectorAll('.'+this.innerText);
         //  manipulating Nearby Locations
-        classRemove (nearbyLocations , 'active-slider');
-        classAdd (nearbyLocations, 'not-active-slider');
+        myClass.removeCla(nearbyLocations , 'active-slider');
+        myClass.addCla(nearbyLocations, 'not-active-slider');
         // manipulating selected slider
-        classRemove (activeNearbyLocation , 'not-active-slider');
-        classAdd (activeNearbyLocation , 'active-slider');
-        console.log(nearbyLocations);
+        myClass.removeCla(activeNearbyLocation , 'not-active-slider');
+        myClass.addCla(activeNearbyLocation , 'active-slider');
     });
   };
 
 
-//   ------class add function
-  function classRemove (elements , classes) {
-      for (let i = 0; i < elements.length; i++) {
-          elements[i].classList.remove(classes);
-      };
-  };
+  // class add and remove Object 
 
-//   --remove class function
-  function classAdd (elements , classes) {
-    for (let i = 0; i < elements.length; i++) {
-        elements[i].classList.add(classes);
+  const myClass = {
+    addCla: function(ele , cla){
+      for (let i = 0; i < ele.length; i++) {
+        ele[i].classList.add(cla);
     };
-  }
+    },
+    removeCla: function(ele , cla){
+      for (let i = 0; i < ele.length; i++) {
+        ele[i].classList.remove(cla);
+    };
+    },
+  };
